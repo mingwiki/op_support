720ff94fc2918f5f6703e4b2d0834f4e50e48374
Fu Ming <mingwiki@gmail.com>
2022-06-10 19:10:41 +0800

    udpate test

--- pages/api/test.js
+++ pages/api/test.js
@@ -2,12 +2,9 @@ import SQL from '../../db'
 
 export default async (req, res) => {
   try {
-    console.log('req body', req.body)
     const result = await SQL({
-      query: 'SELECT * FROM hello_test LIMIT 100',
-      // values: [req.body.content],
+      sql: 'SELECT content FROM hello_test LIMIT 1',
     })
-    console.log('result', result)
     res.status(200).json(result)
   } catch (error) {
     console.log(error)
