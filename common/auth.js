3fa764dd118f15ff245b70df4eaea30ef6f758c4
mingwiki <mingwiki@gmail.com>
2022-08-22 18:50:26 +0800

    formate

--- common/auth.js
+++ common/auth.js
@@ -3,12 +3,12 @@ import SQL from 'common/db'
 export const auth = async (req) => {
   const sessionToken = parseSessionTokenFromCookie(req)
   const check = await SQL(
-    `SELECT COUNT(sessionId) FROM users` +
+    'SELECT COUNT(sessionId) FROM users' +
       (Object.keys(sessionToken)?.length
         ? ` WHERE ${Object.entries(sessionToken)
             .flatMap((e) => `${e[0]}='${e[1]}'`)
             .join(' AND ')}`
-        : null),
+        : null)
   )
   return check[0]['COUNT(sessionId)'] === 1
 }
