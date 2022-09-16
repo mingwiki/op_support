fbf01694da019cf8afe6b93a6a5c52b667c198fd
mingwiki <mingwiki@gmail.com>
2022-09-16 13:09:55 +0800

    parse error

--- common/parse.js
+++ common/parse.js
@@ -1,12 +1,12 @@
 export const parseCookie = (req) => {
   return Object.fromEntries(
-    req.headers?.cookie?.split('; ').map((e) => e.split('='))
+    req.headers?.cookie?.split('; ').map((e) => e.split('=')),
   )
 }
 
 export const parseSessionTokenFromCookie = (req) => {
   const { sessionToken } = Object.fromEntries(
-    req.headers?.cookie?.split('; ').map((e) => e.split('='))
+    req.headers?.cookie?.split('; ').map((e) => e.split('=')),
   )
-  return JSON.parse(sessionToken)
+  return JSON.parse(sessionToken || '{}')
 }
