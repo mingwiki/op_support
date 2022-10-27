54bbb9a393c5db4b6ccc99eaea4fac958c199b9c
mingwiki <mingwiki@gmail.com>
2022-10-27 14:44:09 +0800

    update

--- common/parse.js
+++ common/parse.js
@@ -1,9 +1,3 @@
-export const parseCookie = (req) => {
-  return Object.fromEntries(
-    req.headers?.cookie?.split('; ').map((e) => e.split('=')),
-  )
-}
-
 export const parseSessionTokenFromCookie = (req) => {
   const { sessionToken } = Object.fromEntries(
     req.headers?.cookie?.split('; ')?.map((e) => e.split('=')) || [],
