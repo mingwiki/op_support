a1443b5b3f2948d0e0ccc5498fba687117a8943c
mingwiki <mingwiki@gmail.com>
2023-01-05 19:38:31 +0800

    Update db.js

--- common/db.js
+++ common/db.js
@@ -4,11 +4,11 @@
 import mysql from 'serverless-mysql'
 const db = mysql({
   config: {
-    host: process.env.VITE_MYSQL_HOST,
-    port: process.env.VITE_MYSQL_PORT,
-    database: process.env.VITE_MYSQL_DATABASE,
-    user: process.env.VITE_MYSQL_USER,
-    password: process.env.VITE_MYSQL_PASSWORD,
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    database: process.env.MYSQL_DATABASE,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
   },
 })
 export default async function SQL(sql) {
