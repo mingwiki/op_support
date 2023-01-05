fa423a9080596aa05527584890f7c99fa72f5484
mingwiki <mingwiki@gmail.com>
2023-01-05 19:24:00 +0800

    update

--- common/db.js
+++ common/db.js
@@ -4,17 +4,18 @@
 import mysql from 'serverless-mysql'
 const db = mysql({
   config: {
-    host: process.env.MYSQL_HOST,
-    port: process.env.MYSQL_PORT,
-    database: process.env.MYSQL_DATABASE,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
+    host: import.meta.env.MYSQL_HOST,
+    port: import.meta.env.MYSQL_PORT,
+    database: import.meta.env.MYSQL_DATABASE,
+    user: import.meta.env.MYSQL_USER,
+    password: import.meta.env.MYSQL_PASSWORD,
   },
 })
 export default async function SQL(sql) {
   try {
     console.log(sql)
     const results = await db.query(sql)
+    console.log(results)
     await db.end()
     return results
   } catch (error) {
