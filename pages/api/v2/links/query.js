19a68005c6b551321e63a154b1bf37f2f40e99be
mingwiki <mingwiki@gmail.com>
2023-01-06 15:29:05 +0800

    Update query.js

--- pages/api/v2/links/query.js
+++ pages/api/v2/links/query.js
@@ -14,10 +14,10 @@ export default (req, res) =>
       filter.username = username
     }
     return await SQL(
-      'SELECT * FROM links' +
+      'select a.*, b.appName, c.pageName from links a join applets b on a.appId=b.appId join config c on a.pagePath=c.pagePath' +
         (Object.keys(filter)?.length
           ? ` WHERE ${Object.entries(filter)
-              .flatMap((e) => `${e[0]}='${e[1]}'`)
+              .flatMap((e) => `a.${e[0]}='${e[1]}'`)
               .join(' AND ')}`
           : null) +
         (orderBy ? ` ORDER BY ${orderBy}` : null)
