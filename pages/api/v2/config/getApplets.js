64f61e66030f65f5ace8464fbd3fa1a4201fea00
mingwiki <mingwiki@gmail.com>
2023-01-06 18:18:45 +0800

    Update getApplets.js

--- pages/api/v2/config/getApplets.js
+++ pages/api/v2/config/getApplets.js
@@ -3,5 +3,10 @@ import { request } from '/common/public'
 
 export default (req, res) =>
   request(req, res, async () => {
-    return await SQL('select * from applets')
+    return {
+      applets: await SQL('select * from applets'),
+      config: await SQL(
+        'select a.*, c.id as pageId, c.pageName, c.pagePath, c.hide as pageHide FROM config c  join applets a on a.appId=c.appId'
+      ),
+    }
   })
