9b4efbb0a8297a8eea28b1f413c0c9a77bd98c81
mingwiki <mingwiki@gmail.com>
2023-01-31 13:09:29 +0800

    用户名

--- pages/api/v2/config/updatePreset.js
+++ pages/api/v2/config/updatePreset.js
@@ -1,12 +1,13 @@
 import SQL from '/common/db'
 import { request } from '/common/public'
-
+import { parseSessionTokenFromCookie } from '/common/parse'
 export default (req, res) =>
   request(req, res, async () => {
     const { appId, pagePath, presets } = req.body
+    const { username } = parseSessionTokenFromCookie(req)
     return await SQL(
       `update config set presets='${JSON.stringify(
         JSON.parse(presets)
-      )}' where appId='${appId}' and pagePath='${pagePath}'`
+      )}', username='${username}' where appId='${appId}' and pagePath='${pagePath}'`
     )
   })
