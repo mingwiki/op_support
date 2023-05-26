6a731f5745850c96783ba36cd535ebc1469921a1
mingwiki <mingwiki@gmail.com>
2023-05-26 10:07:49 +0800

    update

--- pages/api/v2/config/all.js
+++ pages/api/v2/config/all.js
@@ -1,9 +1,15 @@
-import SQL from '/common/db'
-import { request } from '/common/public'
+import SQL from "/common/db";
+import { request } from "/common/public";
 
 export default (req, res) =>
   request(req, res, async () => {
     return await SQL(
-      'select a.appId,a.appName,c.* from applets a join config c on a.appId = c.appId where c.hide is null order by c.appId desc, c.id desc'
-    )
-  })
+      `select a.appId, a.appName, c.*
+from applets a
+         join config c on a.appId = c.appId
+where c.hide is null
+group by a.appId, c.pagePath
+order by c.appId desc, c.id desc
+`
+    );
+  });
--- pages/api/v2/users/list.js
+++ pages/api/v2/users/list.js
@@ -1,13 +1,16 @@
-import SQL from '/common/db'
-import { request } from '/common/public'
+import SQL from "/common/db";
+import { request } from "/common/public";
 export default (req, res) =>
   request(
     req,
     res,
     async () => {
       return await await SQL(
-        'SELECT  id, username, nickname, update_time, create_time, disabled FROM users ORDER BY update_time DESC'
-      )
+        `SELECT id, username, nickname, update_time, create_time, disabled
+FROM users
+ORDER BY disabled, update_time DESC
+`
+      );
     },
     true
-  )
+  );
